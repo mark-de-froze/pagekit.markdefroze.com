fb6608cff8e1f42dc570c20eeda1091fe5830721
Mark de Froze <mark.de.froze@mail.ru>
2017-10-30 12:13:26 +0300

    fix API

--- app/controllers/api/v1/site/mails_controller.rb
+++ app/controllers/api/v1/site/mails_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Site::MailsController < ApiController
 
   def create
     mail_params = params.permit(:subject, :message, :from_email, :from_name, :api_key)
-    mail_params[:subject] = 'Call back ' + mail_params[:message] if mail_params[:subject].empty?
+    mail_params[:subject] = 'Call back ' + mail_params[:message] if mail_params[:subject].nil?
     mail_params[:status] = :unread
 
     @mail = Site::Mail.new(mail_params)
