c774f678aa416076f67bb1e690d4358bcaed3f6b
Mark de Froze <mark.de.froze@mail.ru>
2017-11-14 09:23:56 +0200

    fix

--- app/controllers/admin/portfolio/images_controller.rb
+++ app/controllers/admin/portfolio/images_controller.rb
@@ -18,6 +18,7 @@ class Admin::Portfolio::ImagesController < AdminController
     @search = params[:search].to_s.html_safe
 
     @portfolio_images = Portfolio::Image.unscoped
+                            .api(@api_keys_array)
                             .search(:title, @search)
                             .order(updated_at: :desc)
                             .page(params[:page])
